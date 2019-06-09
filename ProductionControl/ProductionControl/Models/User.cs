146ac1539b1c3cc34f8f68348ae758db642c3d87
Cromwill <33699699+Cromwill@users.noreply.github.com>
2019-06-09 21:11:37 +0300

    Create Project

---
+++ ProductionControl/ProductionControl/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductionControl.Models
+{
+    public class User
+    {
+        private string _password;
+
+        public int UserId;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public UserRole Role { get; set; }
+    }
+
+    public enum UserRole
+    {
+        Administrator,
+        User,
+        Official
+    }
+}
